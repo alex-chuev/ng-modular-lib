d649e9ebf627ae613a7e6e49841fc9fa70d28e40
Alex Chuev <a.chuev@itransition.com>
2020-07-25 14:28:16 +0300

    Create apollo lib

---
+++ apollo/src/lib/apollo.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-apollo',
+  template: `
+    <p>
+      apollo works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ApolloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ apollo/src/lib/apollo.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ApolloComponent } from './apollo.component';
+
+
+
+@NgModule({
+  declarations: [ApolloComponent],
+  imports: [
+  ],
+  exports: [ApolloComponent]
+})
+export class ApolloModule { }
